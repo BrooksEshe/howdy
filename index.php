70398f98cfc15021995b94a765cac57d9ed80d48
Brooks Eshe <beshe@mail.greenriver.edu>
2019-01-09 13:35:56 -0500

    Create a fat free index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brook
+ * Date: 1/9/2019
+ * Time: 10:06 AM
+ */
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    echo '<h1>Howdy</h1>';
+    //$view = new View;
+    //echo $view->render('views/home-page.html');
+});
+
+//run fat free
+$f3->run();
+
